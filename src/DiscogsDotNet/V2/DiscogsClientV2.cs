17ab9de0b433b7b1c469d4d183afa4e06b02dc9c
Gowon Patterson <contact@gowon.dev>
2024-11-05 04:40:45 -0500

    feat(client): scaffold out rest client

---
+++ src/DiscogsDotNet/V2/DiscogsClientV2.cs
@@ -0,0 +1,26 @@
+﻿namespace DiscogsDotNet.V2;
+
+using System.Text;
+using Boiler.QuartzWorker.RestApi.Client;
+
+public partial class DiscogsClientV2
+{
+    private Task PrepareRequestAsync(HttpClient client, HttpRequestMessage request, StringBuilder urlBuilder,
+        CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    private Task PrepareRequestAsync(HttpClient client, HttpRequestMessage request, string url,
+        CancellationToken cancellationToken)
+    {
+        request.Headers.TryAddWithoutValidation(Constants.ApiVersionHeader, Constants.V1.ApiVersion);
+        return Task.CompletedTask;
+    }
+
+    private Task ProcessResponseAsync(HttpClient client, HttpResponseMessage response,
+        CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ src/DiscogsDotNet/readme.md
@@ -0,0 +1,8 @@
+﻿# Notes
+
+
+There is a bug in the NSwag client generator where it does not properly handle endpoints that do not have a response defined. This issue needed to be manually fixed after the client was generated.
+
+References:
+
+- <https://github.com/RicoSuter/NSwag/issues/3912>
